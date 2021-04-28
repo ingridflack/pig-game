eec938e12b172e0a218903f0df2ff19a1f0ef0a9
Ingrid Flack <ingrid_flack@hotmail.com>
2021-04-28 18:56:35 -0300

    how to play instructions added

--- script.js
+++ script.js
@@ -103,3 +103,34 @@ btnHold.addEventListener("click", function () {
 btnNew.addEventListener("click", function () {
   init();
 });
+
+// Modal
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelector(".btn-show-modal");
+
+btnOpenModal.addEventListener("click", function () {
+  openModal();
+});
+
+console.log(btnOpenModal);
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
